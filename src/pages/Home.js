74a25da56cf1aa39fac4bf7f3dba514c765a12a0
Jesse Meirink <jesse@meirink.com>
2022-09-22 16:05:21 -0500

    connect to backend using useEffect to fetch data and apply styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react"
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
+
+  // useEffect fires a function when the component is rendered (this is allowing us to fetch data from the backend)
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts")
+      const json = await response.json()
+
+      if (response.ok) {
+        setWorkouts(json)
+      }
+    }
+
+    fetchWorkouts()
+  }, [])
+
   return (
     <div className="home">
-      <h1>Home</h1>
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <WorkoutDetails key={workout._id} workout={workout}/>
+        ))}
+      </div>
     </div>
   )
 }
